c49775e6b5770a6308f26fb3ee4722cae92dfbaa
NoobRushEr <dipeshghag1234@gmail.com>
2022-04-05 23:09:17 +0530

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/header/header/header.component.css 	new file:   src/app/header/header/header.component.html 	new file:   src/app/header/header/header.component.spec.ts 	new file:   src/app/header/header/header.component.ts 	new file:   src/app/posts/post-create/post-create.component.css 	new file:   src/app/posts/post-create/post-create.component.html 	new file:   src/app/posts/post-create/post-create.component.spec.ts 	new file:   src/app/posts/post-create/post-create.component.ts 	new file:   src/app/posts/post-list/post-list.component.css 	new file:   src/app/posts/post-list/post-list.component.html new file:   src/app/posts/post-list/post-list.component.spec.ts new file:   src/app/posts/post-list/post-list.component.ts new file:   src/app/posts/post.model.ts new file:   src/app/posts/posts.service.spec.ts new file:   src/app/posts/posts.service.ts modified:   src/index.html modified:   src/styles.css modified:   tsconfig.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from'@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatExpansionModule
+} from '@angular/material/expansion';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostsService } from './posts/posts.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatExpansionModule
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
